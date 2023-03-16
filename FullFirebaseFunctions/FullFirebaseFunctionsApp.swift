9440e51aea6863fb39066e8ff42314f0f24199bc
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2023-03-17 00:09:34 +0300

    Initial Commit

---
+++ FullFirebaseFunctions/FullFirebaseFunctionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FullFirebaseFunctionsApp.swift
+//  FullFirebaseFunctions
+//
+//  Created by Alexander Sobolev on 17.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct FullFirebaseFunctionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
